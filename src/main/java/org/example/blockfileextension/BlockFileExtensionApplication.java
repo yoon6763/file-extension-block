3dd9e9247b25203cdd6ef1e7dffbbd64d78ba299
uknow-home <yoon6763@naver.com>
2024-02-02 12:18:24 +0900

    initial commit

---
+++ src/main/java/org/example/blockfileextension/BlockFileExtensionApplication.java
@@ -0,0 +1,13 @@
+package org.example.blockfileextension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockFileExtensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockFileExtensionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/blockfileextension/BlockFileExtensionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.blockfileextension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockFileExtensionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
